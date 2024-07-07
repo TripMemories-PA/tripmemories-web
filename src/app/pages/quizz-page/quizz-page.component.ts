478b1f4df5b944f2c5bfd16e57d4857e5a115d5b
Emmanuel <manuboisset@gmail.com>
2024-07-07 19:33:27 +0200

    answer quiz okay

--- src/app/pages/quizz-page/quizz-page.component.ts
+++ src/app/pages/quizz-page/quizz-page.component.ts
@@ -7,23 +7,34 @@ import { QuestionModel } from '../../models/question.model';
 import { QuestionComponent } from '../../components/question/question.component';
 import { NgIf } from '@angular/common';
 import { WelcomeQuizzComponent } from '../../components/welcome-quizz/welcome-quizz.component';
+import { EndQuizComponent } from '../../components/end-quiz/end-quiz.component';
+import { SummaryQuizModel } from '../../models/summaryQuiz.model';
 
 @Component({
     selector: 'app-quizz-page',
     standalone: true,
-    imports: [RouterLink, InputTextModule, QuestionComponent, NgIf, WelcomeQuizzComponent],
+    imports: [
+        RouterLink,
+        InputTextModule,
+        QuestionComponent,
+        NgIf,
+        WelcomeQuizzComponent,
+        EndQuizComponent,
+    ],
     templateUrl: './quizz-page.component.html',
     styleUrl: './quizz-page.component.css',
 })
 export class QuizzPageComponent implements OnInit {
     constructor(
         private authService: AuthService,
-        private router: Router,
         private _activatedRoute: ActivatedRoute,
         private poiService: PoisService,
     ) {}
 
-    quizzStarted = false;
+    summary: SummaryQuizModel[] = [];
+
+    endQuiz = false;
+    quizStarted = false;
     indexQuestion = 0;
     name = this.authService.user?.firstname + ' ' + this.authService.user?.lastname;
     idQuizz = '';
@@ -34,7 +45,7 @@ export class QuizzPageComponent implements OnInit {
 
     startQuiz() {
         //this.router.navigate(['/quizz', this.idQuizz, '1']);
-        this.quizzStarted = true;
+        this.quizStarted = true;
         this.question = this.questions[0];
     }
 
@@ -82,6 +93,19 @@ export class QuizzPageComponent implements OnInit {
             this.score -= 5;
         }
         this.indexQuestion += 1;
+        if (this.indexQuestion >= this.nbrQuestions) {
+            this.endQuiz = true;
+            this.quizStarted = false;
+            this.summary.push({
+                question: this.question,
+                isCorrect: isCorrect,
+            });
+            return;
+        }
+        this.summary.push({
+            question: this.question,
+            isCorrect: isCorrect,
+        });
         this.question = this.questions[this.indexQuestion];
     }
 }

5dc8ce8238e94113b5b094e4f7ef5fad5806af1e
Emmanuel <manuboisset@gmail.com>
2024-07-09 00:51:41 +0200

    add timer quiz

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    OnChanges,
+    OnInit,
+    Output,
+    SimpleChanges,
+} from '@angular/core';
 import { ProgressBarModule } from 'primeng/progressbar';
 import { NgClass, NgForOf, NgIf, NgOptimizedImage, Location } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
@@ -24,7 +32,7 @@ import { MessageService } from 'primeng/api';
     templateUrl: './question.component.html',
     styleUrl: './question.component.css',
 })
-export class QuestionComponent implements OnInit {
+export class QuestionComponent implements OnInit, OnChanges {
     constructor(
         private _location: Location,
         private quizzService: QuizzService,
@@ -35,6 +43,8 @@ export class QuestionComponent implements OnInit {
     selectedAnswer: number = -1;
     hasValidate = false;
     isCorrect = false;
+    totalScore = 0;
+    timer = 0;
 
     @Input() nbrQuestion = 1;
     @Input() totalQuestions = 10;
@@ -46,6 +56,36 @@ export class QuestionComponent implements OnInit {
         if (!this.questionModel) {
             this._location.back();
         }
+        if (this.authService.user?.score) {
+            this.totalScore = this.authService.user.score;
+        }
+    }
+
+    ngOnChanges(_: SimpleChanges): void {
+        setTimeout(() => {
+            this.updateTimer();
+        }, 1500);
+    }
+
+    private updateTimer() {
+        const interval = setInterval(() => {
+            if (this.timer === 10) {
+                clearInterval(interval);
+                this.timer = 0;
+                if (this.selectedAnswer === -1) {
+                    this.messageService.add({
+                        severity: 'error',
+                        summary: "Mince tu n'as pas répondu à temps !",
+                        detail: "Ne t'inquiète pas tu peux te rattraper !",
+                    });
+                    this.nextQuestion.emit(false);
+                } else {
+                    this.goNextQuestion();
+                }
+            } else {
+                this.timer += 1;
+            }
+        }, 1000);
     }
 
     chooseAnswer(index: number) {
@@ -82,8 +122,9 @@ export class QuestionComponent implements OnInit {
                     setTimeout(() => {
                         this.hasValidate = false;
                         this.selectedAnswer = -1;
+                        this.totalScore += response.isCorrect ? 10 : -5;
                         this.nextQuestion.emit(response.isCorrect);
-                    }, 5000);
+                    }, 2000);
                 },
                 error: (error) => {
                     console.log(error);

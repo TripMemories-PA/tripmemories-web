7de1af6c0624a69123c15dcd3fdd4918a48a3307
Emmanuel Boisset <72619789+ShadowZ11@users.noreply.github.com>
2024-07-13 19:24:16 +0200

    update payment card ok (#59)

--- src/app/components/payment-card/payment-card.component.spec.ts
+++ src/app/components/payment-card/payment-card.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PaymentCardComponent } from './payment-card.component';
 import { NgxStripeModule } from 'ngx-stripe';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('PaymentCardComponent', () => {
     let component: PaymentCardComponent;
@@ -9,7 +10,11 @@ describe('PaymentCardComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [PaymentCardComponent, NgxStripeModule.forRoot('pk_test_51J4J5eL8')],
+            imports: [
+                PaymentCardComponent,
+                NgxStripeModule.forRoot('pk_test_51J4J5eL8'),
+                HttpClientModule,
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(PaymentCardComponent);

9bb5ad488b28f105f41630f6cae202522eb83504
Emmanuel <manuboisset@gmail.com>
2024-07-13 14:45:18 +0200

    add speed time analytics vercel

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { AppComponent } from './app/app.component';
 import { inject } from '@vercel/analytics';
 import { environment } from './environments/environment';
 import Pusher from 'pusher-js';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 if (environment.production) {
     inject();
+    injectSpeedInsights();
 }
 
 if (!environment.production) {

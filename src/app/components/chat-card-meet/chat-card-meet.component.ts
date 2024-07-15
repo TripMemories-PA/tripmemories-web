7ab4790cda12494d70f09ce5bbaab144ac673876
Emmanuel <manuboisset@gmail.com>
2024-07-15 21:46:21 +0200

    fix meet page problems

--- src/app/components/chat-card-meet/chat-card-meet.component.ts
+++ src/app/components/chat-card-meet/chat-card-meet.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-import { NgOptimizedImage } from '@angular/common';
+import { NgIf, NgOptimizedImage } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-chat-card-meet',
     standalone: true,
-    imports: [ButtonModule, NgOptimizedImage],
+    imports: [ButtonModule, NgOptimizedImage, NgIf],
     templateUrl: './chat-card-meet.component.html',
     styleUrl: './chat-card-meet.component.css',
 })
@@ -18,6 +18,7 @@ export class ChatCardMeetComponent {
     @Input() nbrMessages: number = 0;
     @Input() nbrMembres: number = 0;
     @Input() meetId: string = '';
+    @Input() hasJoined: boolean = false;
 
     goToChat() {
         this.router.navigate([`/meets/conversations`, this.meetId]);

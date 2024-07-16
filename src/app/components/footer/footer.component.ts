1fb7f5003ca52bc013fcecab187f6951937daab3
Emmanuel <manuboisset@gmail.com>
2024-07-16 20:33:30 +0200

    adding redirection to mobile apps footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
     selector: 'app-footer',
     standalone: true,
-    imports: [],
+    imports: [NgOptimizedImage],
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.css',
 })
 export class FooterComponent {
     fullYear: number = new Date().getFullYear();
+
+    navigateTo(route: string) {
+        window.open(route, '_blank');
+    }
 }

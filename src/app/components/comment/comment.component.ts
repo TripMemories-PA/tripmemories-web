d7dd5f40992c368adb23b9376e28d9194f160f34
Emmanuel Boisset <72619789+ShadowZ11@users.noreply.github.com>
2024-07-17 02:53:39 +0200

    repot comments and posts (#82)
    * repot comments and posts
    
    * update process messages function conversation

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -4,14 +4,29 @@ import { PanelModule } from 'primeng/panel';
 import { ButtonModule } from 'primeng/button';
 import { User } from '../../models/user';
 import { TimeAgoPipe } from '../../time-ago.pipe';
-import { NgIf } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { CommentsService } from '../../services/comments/comments.service';
 import { MessageService } from 'primeng/api';
+import { DialogModule } from 'primeng/dialog';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { ToastModule } from 'primeng/toast';
+import { MessageModule } from 'primeng/message';
 
 @Component({
     selector: 'app-comment',
     standalone: true,
-    imports: [AvatarModule, PanelModule, ButtonModule, TimeAgoPipe, NgIf],
+    imports: [
+        AvatarModule,
+        PanelModule,
+        ButtonModule,
+        TimeAgoPipe,
+        NgIf,
+        DialogModule,
+        ProgressBarModule,
+        ToastModule,
+        MessageModule,
+        NgClass,
+    ],
     providers: [MessageService],
     templateUrl: './comment.component.html',
     styleUrl: './comment.component.css',
@@ -23,8 +38,13 @@ export class CommentComponent {
     @Input() isMyComment?: boolean;
     @Input() author?: User;
     @Input() nbrLikes?: number;
+    @Input() hasBeenReported?: boolean = false;
     @Input() date?: Date;
     @Output() emitter: EventEmitter<any> = new EventEmitter();
+
+    visibleReportDialog: boolean = false;
+    loading: boolean = false;
+    success: boolean = false;
 
     constructor(
         private commentsService: CommentsService,
@@ -55,6 +75,27 @@ export class CommentComponent {
                 next: (_) => {
                     this.emitter.emit(true);
                     this.isLiked = false;
+                },
+                error: (error) => {
+                    this.messageService.add({
+                        severity: 'error',
+                        summary: 'Erreur',
+                        detail: error.error.message,
+                    });
+                },
+            });
+        }
+    }
+
+    reportComment() {
+        if (this.commentId) {
+            this.commentsService.reportComment(this.commentId).subscribe({
+                next: (_) => {
+                    this.success = true;
+                    setTimeout(() => {
+                        this.visibleReportDialog = false;
+                        this.emitter.emit(true);
+                    }, 3000);
                 },
                 error: (error) => {
                     this.messageService.add({
